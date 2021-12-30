3ec0c2ab4edeaa3311fb4960312ce004d66a9b7d
Anuj Gupta <apgupta3091@gmail.com>
2021-12-30 15:56:17 -0500

    finished frontend auth

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,13 @@
+import { combineReducers } from "redux";
+import errorsReducer from "./errors_reducer";
+import sessionsReducer from "./session_reducer";
+import entitiesReducer from "./entities_reducer";
+
+
+const rootReducer = combineReducers({
+    entities: entitiesReducer,
+    session: sessionsReducer,
+    errors: errorsReducer
+});
+
+export default rootReducer;
--- frontend/soundwave.jsx
+++ frontend/soundwave.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from './store/store';
+// import { postUser, postSession, deleteSession } from './util/session_api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
+  // window.postUser =postUser;
+  // window.postSession = postSession;
+  // window.deleteSession = deleteSession;
   const root = document.getElementById("root");
+  const store = configureStore();
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
   ReactDOM.render(<h1>Welcome to SoundWave</h1>, root);
 });
