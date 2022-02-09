c62e045c404f06638c2f3272ff5aa36ca6c583d8
Anuj Gupta <apgupta3091@gmail.com>
2022-02-09 17:41:26 -0500

    hi

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,7 +1,7 @@
 class Api::CommentsController < ApplicationController
     def create
         @comment = Comment.new(comment_params)
-        # @comment.commenter_id = current_user.id
+        @comment.commenter_id = current_user.id
 
         if @comment.save
             render "api/comments/show"
